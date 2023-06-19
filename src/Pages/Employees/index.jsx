bae177181b5696c93b2d9ee7270ee58fc6a9f669
Ivan42210 <ivan.cha.42@outlook.fr>
2023-06-19 21:15:44 +0200

    data table plug in

--- src/Pages/Employees/index.jsx
+++ src/Pages/Employees/index.jsx
@@ -1,8 +1,9 @@
+import './Employees.css'
+
 export default function Employees(){
     return(
-        <>
-
-            Employees
-        </>
+        <main className="employees-page">
+            <h2>Current Employees</h2>
+        </main>
     )
 }
